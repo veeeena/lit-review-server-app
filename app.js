cccaaabaee5d6ca7bf64f781b20488fa39b70f90
Veena K <vkodakirthi@gmail.com>
2023-12-10 23:05:38 -0500

    process env

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ import BookRoutes from "./books/routes.js";
 import ReviewRoutes from "./reviews/routes.js";
 import ReviewBookRoutes from "./review-books/routes.js";
 import session from "express-session";
+import "dotenv/config";
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/lit-review';
 mongoose.connect(CONNECTION_STRING);
+
+console.log(process.env.FRONTEND_URL)
 
 const app = express()
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000"
+    origin: process.env.FRONTEND_URL
   })
 ); 
 const sessionOptions = {
@@ -22,9 +25,14 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
-app.use(
-  session(sessionOptions)
-);
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
 app.get('/', (req, res) => {res.send('welcome to the lit review api!')})
 app.use(express.json());
 UserRoutes(app);
